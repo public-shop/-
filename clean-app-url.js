edecd9ff6f339b259609d9ead434a83c3c65c1ed
public-shop <kongespresso3@gmail.com>
2025-02-14 00:05:27 +0700

    Update clean-app-url.js

--- clean-app-url.js
+++ clean-app-url.js
@@ -13,9 +13,9 @@
     // ถ้ามีการลบพารามิเตอร์ออก
     if (hasRemoved) {
         // ตรวจสอบว่า URL ไม่ใช่ https://public-shop.github.io/dashboard/
-        if (url.href !== "https://public-shop.github.io/dashboard/index.html") {
+        if (url.href !== "https://public-shop.github.io/dashboard") {
             // ถ้าไม่ใช่ URL ที่ต้องการ ให้เปลี่ยนเส้นทาง
-            window.location.href = "https://public-shop.github.io/dashboard/index.html";
+            window.location.href = "https://public-shop.github.io/dashboard";
         } else {
             // ถ้าเป็น URL ที่ต้องการ ให้เปลี่ยน URL โดยไม่รีเฟรช
             window.history.replaceState({}, document.title, url.pathname + url.search);
